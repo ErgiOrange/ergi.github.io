f66b995704cfc5cb948999c3e95c4aa44c32959d
ErgiOrange <63922379+ErgiOrange@users.noreply.github.com>
2020-04-19 14:37:48 +0200

    Create postcomment.php

---
+++ comments/postcomment.php
@@ -0,0 +1,24 @@
+*/
+	if ($_POST['page'] != '' && $_POST['name'] != '' && $_POST['comment'] != '' && file_exists($_POST['page'].".php")) {
+		$name = str_replace("~", "", $_POST['name']);
+		$comment = str_replace("~", "", $_POST['comment']);
+		
+		$name = strip_tags($name);
+		$comment = strip_tags($comment);
+
+		$comment = nl2br($comment, TRUE);
+		$comment = str_replace(array("\r\n", "\r", "\n"), "", $comment);
+		$name = substr($name, 0, 100);
+		$comment = substr($comment, 0, 5000);
+		
+		$tfile = $_POST['page']."c.txt";
+		if (file_exists($tfile)) {
+			$str = file_get_contents($tfile)."\n";
+		} else {
+			$str = '';
+		}
+		$str .= $name."~".time()."~".$comment;
+		file_put_contents($tfile, $str);
+	}
+	header("Location: ". $_POST['page'].".php");
+?>
